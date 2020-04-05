d1108ce15772dcf9ec4457ffe0dd366d2a7e4142
jayaharish <61152197+jayaharish@users.noreply.github.com>
2020-04-05 13:22:10 +0530

    with build

---
+++ build/precache-manifest.c102ac666dc6945c054fe4910a578d6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ef79078ba2a120127cdbfe0c2e70847",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7df9878ff219f5c9d1bd",
+    "url": "/static/css/main.6a872407.chunk.css"
+  },
+  {
+    "revision": "8b5d100ab8d1dd91cf07",
+    "url": "/static/js/2.065bb7d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.065bb7d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7df9878ff219f5c9d1bd",
+    "url": "/static/js/main.be1deb52.chunk.js"
+  },
+  {
+    "revision": "9290df0cd23318772556",
+    "url": "/static/js/runtime-main.ba424f99.js"
+  }
+]);
